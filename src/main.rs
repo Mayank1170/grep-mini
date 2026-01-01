e7a79fe7b1fd79ebbdf560a9d6da302c1de5e78a
Mayank1170 <agarwalmayank1170@gmail.com>
2026-01-01 20:01:36 +0530

    feat: added case sensitive searching uing environment variables

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::fs;
 use std::{env, process};
 
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -28,7 +28,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
 
@@ -38,6 +44,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 struct Config {
     query: String,
     file_path: String,
+    pub ignore_case: bool,
 }
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
@@ -46,7 +53,11 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
